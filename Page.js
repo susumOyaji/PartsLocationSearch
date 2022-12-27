db9a660e28c33a339ee29040cb78e2218270cba7
susumOyaji <sumitomo0210@gmail.com>
2022-12-27 17:37:20 +0900

    up

---
+++ Page.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+(function () {
+
+
+
+
+
+    
+    const eOutput = document.querySelector('#test-output');
+
+    document.querySelector('#btn-clear-log').addEventListener('click',function(){
+        eOutput.innerText = '';
+    });
+
+})();
