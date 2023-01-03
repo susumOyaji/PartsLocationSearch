effaeebb1f6164179ea440ef269a7c6a8c296e55
susumOyaji <sumitomo0210@gmail.com>
2023-01-03 10:12:36 +0900

    up

--- PopUp.js
+++ PopUp.js
@@ -9,3 +9,18 @@ popup.addEventListener('click', () => {
 close.addEventListener('click', () => {
     popWin.style.display = 'none';
 });
+
+
+$(function() {
+
+    // show popupボタンクリック時の処理
+    $('#show').click(function(e) {
+        $('#popup, #layer').show();
+    });
+
+    // レイヤー/ポップアップのcloseボタンクリック時の処理
+    $('#close, #layer').click(function(e) {
+        $('#popup, #layer').hide();
+    });
+
+});
